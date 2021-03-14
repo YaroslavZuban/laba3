3bf039ae4a8e837b416ea64646d421862143ffce
Zuban Yaroslav <yaroslav.zuban7029@gmail.com>
2021-03-14 19:58:16 +0700

    Все вроде сделал не знаю как сделать 6,11

--- Zadanie3.py
+++ Zadanie3.py
@@ -1,3 +1,3 @@
 from IPython.display import display, Math
 display(Math(r'F(k) = \int_{-\infty}^{\infty} sin(x)*cos(x) dx'))
-#Код работает в jupyter notebook, так что смело копируй и вставляй!
+#Код работает в jupyter notebook, так что смело копируй и вставляй
