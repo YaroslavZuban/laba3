50e9e7144550c07cf0cec56765e2c26d92c06de4
Zuban Yaroslav <yaroslav.zuban7029@gmail.com>
2021-03-14 16:47:42 +0700

    сделал 1.2.3.4.5.6.7 пункт

--- Zadanie1.py
+++ Zadanie1.py
@@ -1,1 +1,24 @@
-print("привет")
+import pandas as pd
+import numpy
+import matplotlib.pyplot as plt
+
+df = pd.DataFrame({
+    'Студент': ['Степан Белов', 'Сергей Даренко', 'Ира Белоглазова', 'Вика Марченко', 'Илья Горин'],
+    'Курс': [2, 3, 1, 4, 2],
+    'Факультет': ['РЭФ', 'РЭФ', 'ФЛА', 'ФМА', 'ФПМИ']
+})
+
+print(df, '\n')
+
+df['Средняя оценка'] = [3.5, 2.4, 4.6, 4.2, 5]
+print(df, '\n')
+pf = df
+
+df = df.groupby('Студент').mean()
+print(df, '\n')
+
+df = df.reset_index(drop=False)
+print(df, '\n')
+
+table = pd.pivot_table(pf, values='Средняя оценка', index=['Факультет', 'Студент'], aggfunc=numpy.sum)
+print(table, '\n')
